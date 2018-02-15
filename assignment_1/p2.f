5285425bc4fe3f268e91a8790e4c0e29990563fb
Austin Ray <austin.duke.ray@gmail.com>
2018-02-14 23:24:11 -0500

    Remove extra leading space to make to Fortran
    Removed extra leading spaces in an attempt to make GitHub identify p2.f
    as Fortran instead of Fourth.

--- assignment_1/p2.f
+++ assignment_1/p2.f
@@ -1,20 +1,20 @@
-          program main
-              integer i, pass_down/0/, show_staticness
+      program main
+        integer i, pass_down/0/, show_staticness
 
-              do 10 i = 1, 10
-                pass_down = show_staticness(pass_down + 1)
-10            continue
+        do 10 i = 1, 10
+          pass_down = show_staticness(pass_down + 1)
+10      continue
 
-              stop  'End of program'
-          end
+        stop  'End of program'
+      end
 
-          integer function show_staticness(pass_down)
-              integer pass_down, count/0/
-              write(*,*) "PARAMETER: ", pass_down, "LOCAL: ", count
+      integer function show_staticness(pass_down)
+        integer pass_down, count/0/
+        write(*,*) "PARAMETER: ", pass_down, "LOCAL: ", count
 
-              count           = count     + 1
-              pass_down       = pass_down + 1
-              show_staticness = pass_down 
+        count           = count     + 1
+        pass_down       = pass_down + 1
+        show_staticness = pass_down 
 
-              return 
-          end
+        return 
+      end
