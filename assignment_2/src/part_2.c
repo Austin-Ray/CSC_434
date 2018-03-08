1d6f3c27e8241390475f23e1264d3fc579186a49
Austin Ray <austin@austinray.io>
2018-03-08 15:17:00 -0500

    Complete part_2
    Completed the second part to the assignment. Documentation will need to
    added, but it's written within idiomatic C, which is understandable by C
    developers.

--- assignment_2/src/part_2.c
+++ assignment_2/src/part_2.c
@@ -1,4 +1,19 @@
+#include <stdio.h>
+
+long long fact(long long n);
+
 int main() {
+  fact(6);
+
   return 0;
+}
+
+long long fact(long long n) {
+  printf("Address of n = %p,\tValue of n = %lld\n", (void *)&n, n);
+
+  if (n == 1)
+    return 1;
+
+  return fact(n - 1);
 }
 
