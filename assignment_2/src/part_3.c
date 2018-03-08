5992242dba2b90ac5eee7afc60e3ec09ff2cd9c7
Austin Ray <austin@austinray.io>
2018-03-08 15:18:04 -0500

    Complete part_3
    Completed part_3 of the assignment. Documentation will need to be added.

--- assignment_2/src/part_3.c
+++ assignment_2/src/part_3.c
@@ -1,3 +1,19 @@
+#include <stdio.h>
+
 int main() {
+  int d1 = 4;
+  int d2 = 2;
+  int d3 = 3;
+
+  double array[d1][d2][d3];
+
+  for (int i = 0; i < d1; i++) {
+    for (int j = 0; j < d2; j++) {
+      for (int k = 0; k < d3; k++) {
+        printf("Memory location for (%d,%d,%d) = %p\n", i, j, k, (void *)(&array[i][j][k]));
+      }
+    }
+  }
+
   return 0;
 }
