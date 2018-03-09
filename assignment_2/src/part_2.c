78f5c5b1e150d524e64e121454fca05cb84a3561
Austin Ray <austin@austinray.io>
2018-03-08 20:47:40 -0500

    Document part_2.c with Doxygen style

--- assignment_2/src/part_2.c
+++ assignment_2/src/part_2.c
@@ -1,13 +1,26 @@
 #include <stdio.h>
 
+/**
+ * Compute a factorial
+ *
+ * This function is to demonstrate how memory is allocated in a recursively
+ *
+ * @param  Number of iterations for a factorial.
+ */
 long long fact(long long n);
 
+/**
+ * Entry point in the program
+ *
+ * @return  Exit code
+ */
 int main() {
   fact(6);
 
   return 0;
 }
 
+/* !copydoc fact() */
 long long fact(long long n) {
   printf("Address of n = %p,\tValue of n = %lld\n", (void *)&n, n);
 
